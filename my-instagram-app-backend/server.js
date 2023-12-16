d41efd7e2f1cd505c3cab8e0d257d33107b177d9
skalidindi53 <s.teja2004@gmail.com>
2023-12-16 12:29:10 -0600

    updated port

--- my-instagram-app-backend/server.js
+++ my-instagram-app-backend/server.js
@@ -51,6 +51,7 @@ const compareFollowers = (followers, following) => {
     return followingUsernames.filter(username => !followerUsernames.includes(username));
 };
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
